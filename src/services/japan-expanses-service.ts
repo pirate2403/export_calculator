5103bd33f0258f16c0c1e3720c2b467aa5a70edb
Anatol b <a.birtanov@takeoff-staff.ru>
2024-11-04 02:36:39 +0800

    init

--- src/services/japan-expanses-service.ts
+++ src/services/japan-expanses-service.ts
@@ -1,4 +1,5 @@
 import {Car} from "../interfaces";
+import {ENGINE_TYPE} from "../constants";
 
 export interface JapanExpansesConfig {
     PORT_DELIVERY: number;
@@ -14,9 +15,13 @@ export class JapanExpansesService {
     }
 
     calculate(car: Car): number {
-        const freightIndex = Number(this._config.SANCTION_CONDITIONS.engineVolume <= car.engineVolume);
+        const freightIndex = Number(this._isSanction(car));
         const freight = this._config.FREIGHT[freightIndex];
         const delivery = this._config.PORT_DELIVERY;
         return freight + delivery;
+    }
+
+    private _isSanction(car: Car): boolean {
+        return this._config.SANCTION_CONDITIONS.engineVolume < car.engineVolume || car.engineType === ENGINE_TYPE.ELECTRIC || car.engineType === ENGINE_TYPE.HYBRID
     }
 }
